f22ef6649ca39a805ef244548108ded72f13ecd7
AnasFi <anas.firari@keiken-digital.com>
2023-06-08 10:30:56 +0000

    added orders job to the cluster

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ kubectl apply -f Config/filebeat-config.yaml
 kubectl apply -f Config/logstash-config.yml
 kubectl apply -f Config/webDAVCreds.yaml
 kubectl apply -f Secrets/sfccpwd.yaml
+kubectl apply -f Config/ocapiCreds.yaml
+kubectl apply -f Config/orderlogstash-config.yml
 #Storage
 kubectl apply -f Storage/es-sc.yaml
 #Deployments
@@ -11,7 +13,12 @@ kubectl apply -f Deployments/webDAV.yaml
 kubectl apply -f Deployments/logstashDep.yml
 kubectl apply -f Deployments/kibana.yaml
 kubectl apply -f Deployments/es-statefulsets.yaml
+kubectl apply -f Deployments/orderlogstash.yml
+kubectl apply -f Deployments/kafka-broker.yaml
+kubectl apply -f Deployments/zookeeper.yaml
+kubectl apply -f Deployments/orderjob.yaml
 #Services
 kubectl apply -f Services/es-service.yaml
 kubectl apply -f Services/logstashSvc.yml
+kubectl apply -f Services/orderlogstashSvc.yml
 
